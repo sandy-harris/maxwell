73748b015703679dfdb4cc46885f010f902efc40
Sandy Harris <sandyinchina@gmail.com>
2016-07-31 17:42:46 -0400

    Major changes for version 2

--- src/maxwell.c
+++ src/maxwell.c
@@ -11,6 +11,14 @@
 	If anyone needs it under another Open Source
 	license, they can contact me.
 
+	This is version 2, 2016
+	The main changes from v1, 2012 are
+		different command-line options
+		constants LO_CLAIM & HI_CLAIM
+		(instead of a different prime for each option)
+		moving actual output from main() to do1k()
+		random choice of delay in do1k()
+
 	Sandy Harris, sandyinchina@gmail.com
 */
 
@@ -32,18 +40,20 @@ int good_num(char *) ;
 
 void usage() ;
 
-/*
-	minimum number of loops
-*/
+/* minimum number of loops */
 #define MIN_LOOPS 3
+
+/* entropy claims */
+#define LO_CLAIM 16
+#define HI_CLAIM 30
 
 /*
 	global variables
 	set in main()
-	used by log_error() and error_exit()
+	used by do1k(), log_error() and error_exit()
 */
 char *prog_name ;
-int user, demon ;
+int user, demon, output, loops, mix, mul ;
 
 /*
 	primes taken from
@@ -51,7 +61,8 @@ int user, demon ;
 
 	used as microsecond delay values
 */
-unsigned primes[] = {41,43,97,101,103,107,109,113,127} ;
+unsigned primes[] = {89, 97, 101, 103, 107} ;	// median 101, mean 99.4
+unsigned smallp[] = {43, 47,  53,  57,  59} ;	// median  53, mean 51.8
 
 /*
 	constants borrowed from SHA-1
@@ -71,183 +82,27 @@ u32 sha_c[] = {0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0} ;
 */
 #define MUL (71+(37<<12)+(11<<19))
 
-int main( int argc, char **argv)
+/*
+	produce one K bit of output
+	32 32-bit words
+*/
+void do1k( unsigned delay, unsigned claim )
 {
-	unsigned a, b, p, delay, limit, loops ;
-	int i, j, out, output, foreground, ret, claim, mul, mix ;
-	char *u, *v ;
+	int i, j, k, ret ;
+	unsigned a, b ;
 
-	/*
-		defaults
-		might be overidden by options
-	*/
-	p = 0 ;			// no paranioa, three loops
-	delay = primes[2] ;
-	limit = 0 ;		// loop forever
-	claim = 30 ;		// entropy claim per output
-	/*
-		defaults
-		always apply except in testing
-	*/
-	demon = 1 ;
-	mix = 1 ;
-	mul = MUL ;
-	/*
-		default unless overriden by demon=1
-		output to standard out
-	*/
-	output = 1 ;
-	foreground = 0 ;
+	// fprintf( stderr, "do1k delay %d claim %d mix %d loops %d\n", delay, claim, mix, loops) ;
 
-	/* argument processing */
-	prog_name = *argv ;
-	switch(argc)	{
-		// no options, use defaults
-		case 1:
-			break ;
-		// one, to four name/value pairs
-		// no check for repeated options
-		// so if same option twice, 2nd is used
-		case 9:
-		case 7:
-		case 5:
-		case 3:
-			for( argc--, argv++ ; argc ; argc -= 2, argv += 2)	{
-				u = argv[0] ;
-				v = argv[1] ;
-				if( good_opt(u) && good_num(v) )
-					a = atoi(v) ;
-				else	usage() ;
-				if( !strcmp(u,"-p") )
-					p = a ;
-				else if( !strcmp(u,"-d") )
-					delay = a ;
-				else if( !strcmp(u,"-s") )
-					limit = a ;
-				else if( !strcmp(u,"-c") )
-					if( a <=32 )
-						claim = a ;
-					else
-						claim = 32 ;
-				else
-					usage() ;
-			}
-			break ;
-		// one of the standalone options
-		case 2:
-			// get the option value
-			i = good_opt(argv[1]) ;
-			// fprintf(stderr, "option -> %c\n", i) ;
-
-			// digit -> loops with no mixing
-			if( isdigit(i) )	{
-				demon = 0 ;
-				limit = 1024 ;
-				mix = 0 ;
-				mul = 1 ;
-				loops = (i -'0') ;
-			}
-			else switch(i)	{
-				// faster variants
-				// reduce claim so random(4) accepts
-				// inputs faster
-				case 'f' :
-					delay = primes[0] ;
-					claim = 16 ;
-					limit = 8 ;
-					break ;
-				case 'g' :
-					delay = primes[1] ;
-					claim = 16 ;
-					limit = 16 ;
-					break ;
-				// as -g but don't daemonize
-				case 'G' :
-					delay = primes[1] ;
-					claim = 16 ;
-					limit = 16 ;
-					foreground = 1;
-					break ;
-				// for the paranoids
-				// -x, -y -z do more loops
-				case 'x' :
-					delay = primes[3] ;
-					p = 2 ;
-					break ;
-				case 'y' :
-					delay = primes[4] ;
-					p = 3 ;
-					break ;
-				case 'z' :
-					delay = primes[5] ;
-					p = 4 ;
-					break ;
-				// options for testing
-				case 'm' :
-					// one megabit
-					limit = 1024 ;
-					demon = 0 ;
-					break ;
-				case 't' :
-					// zero delay, faster test
-					delay = 0 ;
-					demon = 0 ;
-					break ;
-				default:
-					usage() ;
-					break ;
-			}
-			break ;
-		default:
-			usage() ;
-			break ;
-	}
-
-	// documented in getuid(1) as "always successful"
-	// so do not bother checking for failure
-	user = geteuid() ;
-
-	/*
-		set up various things
-	*/
-
-	// not testing and not root: fail
-	if( demon && (user != 0) )	{
-		fprintf(stderr, "%s: must be root to run maxwell(8)\n", prog_name) ;
-		fprintf(stderr, "%s: updates kernel entropy count, so root only\n", prog_name) ;
-		fprintf(stderr, "%s: exception is 'maxwell -[t|m|0-9]' for testing\n", prog_name) ;
-		exit(1) ;
-	}
-
-	// not testing and root
-	if( demon )	{
-		if( (output=open("/dev/random", O_WRONLY)) == -1)
-			error_exit("failed to open /dev/random") ;
-		if( !foreground && daemon(-1, -1) == -1)
-			error_exit("failed to become daemon process") ;
-		openlog( prog_name, (LOG_CONS|LOG_PID), LOG_DAEMON) ;
-	}
-
-	// calculate loops per output
-		loops = MIN_LOOPS + 2*p ;
-
-	// limit is given in K bits
-	// but we need the number of 32-bit chunks
-	if( limit )
-		limit *= 32 ;
-
-	message( "maxwell(8) v 1.2" );
-
-	for( out = 0 ; (limit == 0) || (out < limit) ; out++) {
+	for( i = 0 ; i < 32 ; i++ )	{
 		if( mix )
 			// initialise with a somewhat random constant
 			a = sha_c[t5()] ;
 		else
 			a = 0 ;
 		// main loop; sample & mix
-		for( i = 0 ; i < loops ; i++)	{
+		for( j = 0 ; j < loops ; j++)	{
 			// get 16 samples for entropy
-			for( j = 0 ; j < 16 ; j++ )	{
+			for( k = 0 ; k < 16 ; k++ )	{
 				usleep(delay) ;
 				// mix in a sample
 				// multiplication spreads bits out
@@ -266,8 +121,8 @@ int main( int argc, char **argv)
 		if( mix )
 			a = qht(a) ;
 		// output 32 bits
-		if( (ret = write(output, &a, 4)) != 4)
-			error_exit("write to /dev/random failed") ;
+		if( (ret = write( output, &a, 4)) != 4)
+			error_exit("write to output file failed") ;
 		// update entropy estimate
 		if( demon )
 			// a failure here is logged
@@ -276,6 +131,134 @@ int main( int argc, char **argv)
 			if( (ioctl(output, RNDADDTOENTCNT, claim)) == -1)
 				message("ioctl() failed") ;
 	}
+}
+
+int main( int argc, char **argv)
+{
+	unsigned a, b, delay, limit, *ptr ;
+	int i, p, out, fast, halt ;
+	char *u, *v ;
+
+	/*
+		defaults
+		might be overidden by options
+	*/
+	p = 0 ;			// no paranioa, three loops
+	halt = 0 ;		// loop forever
+	fast = 4 ;		// 4 K bits done quickly
+
+	/*
+		defaults
+		always apply except in testing
+	*/
+	demon = 1 ;
+	mix = 1 ;
+	mul = MUL ;
+	loops = MIN_LOOPS ;
+	/*
+		default unless overriden by demon=1
+		output to standard out
+	*/
+	output = 1 ;
+
+	/* argument processing */
+	prog_name = *argv ;
+	for( argc--, argv++ ; (argc > 0) && (*argv != NULL) ; argc--, argv++ )	{
+		u = *argv ;
+		if( u[0] != '-' )
+			usage() ;
+		switch( u[1] )	{
+			case '0' :
+				message( "-0 option asks for 0 output, makes no sense") ;
+				usage() ;
+			// any digit but zero
+			case '1': case '2': case '3':
+			case '4': case '5': case '6':
+			case '7': case '8': case '9':
+				v = u + 1 ;
+				loops = atoi(v) ;
+				// no mixing for test output
+				mix = 0 ;
+				mul = 1 ;
+				// output to stdout
+				// demon = 0 ;
+				break ;
+			// output to stdout
+			case 's':
+				demon = 0 ;
+				break ;
+			// paranoia level
+			case 'p' :
+				v = argv[1] ;
+				if( (v == NULL) || !good_num(v) )
+					usage() ;
+				p = atoi(v) ;
+				// extra increment to skip over numeric argument
+				argc-- ; argv++ ;
+				break ;
+			// initial fast data in K bits
+			case 'f' :
+				v = argv[1] ;
+				if( (v == NULL) || !good_num(v) )
+					usage() ;
+				fast = atoi(v) ;
+				argc-- ; argv++ ;
+				break ;
+			// halt after K bits
+			case 'h' :
+				v = argv[1] ;
+				if( (v == NULL) || !good_num(v) )
+					usage() ;
+				halt = atoi(v) ;
+				argc-- ; argv++ ;
+				break ;
+			default:
+				usage() ;
+		}
+	}
+
+	// documented in getuid(1) as "always successful"
+	// so do not bother checking for failure
+	user = geteuid() ;
+
+	/*
+		set up various things
+	*/
+
+	// not testing and not root: fail
+	if( demon && (user != 0) )	{
+		fprintf(stderr, "%s: must be root to run maxwell(8)\n", prog_name) ;
+		fprintf(stderr, "%s: updates kernel entropy count, so root only\n", prog_name) ;
+		fprintf(stderr, "%s: exception is 'maxwell -s' for testing\n", prog_name) ;
+		exit(1) ;
+	}
+
+	// not testing and root
+	if( demon )	{
+		if( (output=open("/dev/random", O_WRONLY)) == -1)
+			error_exit("failed to open /dev/random") ;
+		if( daemon(-1, -1) == -1)
+			error_exit("failed to become daemon process") ;
+		openlog( prog_name, (LOG_CONS|LOG_PID), LOG_DAEMON) ;
+	}
+
+	message( "maxwell(8) v 2" );
+
+	// first loop, fast output to fill random(4) pool
+	// uses smaller prime and default loops == MIN_LOOPS for speed
+	// -p option has no effect here; not needed when claiming only LO_CLAIM
+	// halt == 0 means run forever
+	// fprintf( stderr, "fast loop: p %d loops %d halt %d fast %d output %d demon %d\n", p, loops, halt, fast, output, demon ) ;
+	for( i = 0 ; (i < fast) && ((halt == 0) || (i < halt)) ; i++ )
+		do1k( smallp[t5()], LO_CLAIM ) ;
+
+	// main loop, halt == 0 means run forever
+	// -p takes efect here
+	loops = MIN_LOOPS + 2*p ;
+	// fprintf( stderr, "main loop: p %d loops %d halt %d fast %d output %d demon %d\n", p, loops, halt, fast, output, demon ) ;
+	for( /* continue with same i */ ; (halt == 0) || (i < halt) ; i++ )
+		do1k( primes[t5()], HI_CLAIM ) ;
+
 	message( "shutting down" );
 	exit(0) ;
 }
@@ -285,7 +268,7 @@ void error_exit(const char *reason)
 	if( demon )		// running as root
 		syslog(LOG_ERR, "%s", reason) ;
 	else
-		fprintf(stderr, "%s: %s\n", prog_name, reason) ;
+		fprintf(stderr, "%s: error: %s\n", prog_name, reason) ;
 	exit(-1) ;
 }
 
@@ -294,33 +277,18 @@ void message(const char *reason)
 	if( demon )
 		syslog(LOG_INFO, "%s", reason) ;
 	// else
-	//   do nothing
+	// do nothing
 }
 
 void usage()
 {
-	fprintf(stderr, "usage: %s [-p <number>] [-d <number>] [-s <number>] [-c number]\n", prog_name) ;
-	fprintf(stderr, "usage: %s [-f|-g|-G|-m|-t|-x|-y|-z|-<digit>]\n", prog_name) ;
+	fprintf(stderr, "usage: %s [-<digit>] [-s] [-p <number>] [-h <number>] [-f <number>]\n", prog_name) ;
+	fprintf(stderr, "usage: %s <digit> must be in range 1-9, <number> 0-99\n", prog_name) ;
 	exit(-1) ;
 }
 
 /*
-	is this a legal single character option?
-
-	no program-specific checks
-	just is it '-' followed by a single letter or digit
-	if so, return the letter or digit
-*/
-int good_opt(char *p)
-{
-	if( (p[0] == '-') && isalnum(p[1]) && (p[2] == '\0') )
-		return(p[1]) ;
-	else
-		return(0) ;
-}
-
-/*
-	allows numbers 0 - 999, no + - or decimal point
+	allows numbers 0 - 99, no + - or decimal point
 */
 int good_num(char *p)
 {
@@ -332,6 +300,5 @@ int good_num(char *p)
 		else
 			return(0) ;
 	}
-	return( (i>0) && (i<=3) ) ;
+	return( (i>0) && (i<=2) ) ;
 }
-
