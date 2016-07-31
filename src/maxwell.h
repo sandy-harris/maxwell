2f657593413bd9e7f6259ba8728ab802bd5dbae2
Sandy Harris <sandyinchina@gmail.com>
2016-07-31 19:28:01 -0400

    Change declaration of MUL for better mixing

--- src/maxwell.h
+++ src/maxwell.h
@@ -8,11 +8,17 @@ typedef unsigned u32 ;
 /*
 	constant for multiplications
 
-	11, 37 and 71 are 3, 5 and 7 mod 8
+	11, 37, 71 and 41 are 3, 5, 7 and 9 mod 16
 	so they all give some mixing in the low bits
 	and they each do it differently
+
+	These get multiplied by a value taken mod 31
+	so each of the shifted constants can affect
+	a different number of bits. e.g. the 1 can
+	affect only 5 bits, but 71 can give a value
+	up to 31*71 and affect up to 11 bits.
 */
-#define MUL (71+(37<<12)+(11<<19))
+#define MUL (71+(37<<8)+(41<<15)+(11<<19)+(1<<25))
 
 /* forward declarations of functions */
 
